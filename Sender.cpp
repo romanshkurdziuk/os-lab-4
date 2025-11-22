dbae4a48f13d7132dea750d471099f42e8f1ab4e
Roman Shkurdyuk <romansh0106@gmail.com>
2025-11-22 13:19:53 +0300

    step1: чтение и создание бинарного файла

--- Sender.cpp
+++ Sender.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <windows.h>
+#include "fstream"
+#include "message.h"
 
 using namespace std;
 
 int main()
 {
+    string fileName;
+    int recordsCount;
+
+    cout << "Enter binary file name (e.g. data.bin): ";
+    cin >> fileName;
+
+    cout << "Enter records count: ";
+    cin >> recordsCount;
+    
+    ofstream file(fileName, ios::binary | ios::out);
+
+    if (!file.is_open())
+    {
+        cerr << "Error opening file." << endl;
+        return 1;
+    }
+    cout << fileName << "Succesfully opened file" << endl;
 
 
+    file.close();
+
+    cout << "Press enter to exit" << endl;
+    cin.ignore();
+    cin.get();
 
     return 0;
 }
